ab3e0cb4b981556ff59ae68744111834fd930518
zhang <295253797@qq.com>
2016-08-10 14:36:32 +0800

    Initial commit

---
+++ app/src/main/java/com/neu/activity/MyActivity.java
@@ -0,0 +1,17 @@
+package com.neu.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.vector.my12306.R;
+
+/**
+ * Created by zhang on 2016/8/10.
+ */
+public class MyActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+    }
+}
